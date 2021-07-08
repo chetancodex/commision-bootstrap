3315f85801f326773ebf31222acb731aafd41ba7
himanshuchauhan3922 <himanshuchauhan3922@gmail.com>
2021-07-08 17:06:28 +0530

    work to home page

--- src/app/page/life-at-vc-page/life-at-vc-page.module.ts
+++ src/app/page/life-at-vc-page/life-at-vc-page.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { LifeAtVcPageComponent } from './life-at-vc-page.component';
 import { AboutHeroModule } from '../../about/about-hero/about-hero.module';
 import { MaxQualityPerformanceModule } from '../../about/max-quality-performance/max-quality-performance.module';
-import { TeamImagesModule } from '../../vCLifeComponent/team-images/team-images.module';
-import { LastImgWithPreviousNextModule } from '../../vCLifeComponent/last-img-with-previous-next/last-img-with-previous-next.module';
+import { TeamImagesModule } from '../../vc-life-component/team-images/team-images.module';
+import { LastImgWithPreviousNextModule } from '../../vc-life-component/last-img-with-previous-next/last-img-with-previous-next.module';
 
 
 
--- src/app/page/partners-page/partners-page.module.ts
+++ src/app/page/partners-page/partners-page.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PartnersPageComponent } from './partners-page.component';
 import { AboutHeroModule } from '../../about/about-hero/about-hero.module';
-import { CompanyListModule } from '../../partnersComponent/company-list/company-list.module';
-import { LastImgWithPreviousNextModule } from 'src/app/vCLifeComponent/last-img-with-previous-next/last-img-with-previous-next.module';
+import { CompanyListModule } from '../../partners-component/company-list/company-list.module';
+import { LastImgWithPreviousNextModule } from 'src/app/vc-life-component/last-img-with-previous-next/last-img-with-previous-next.module';
 
 
 
