bf98065b7af998822a799b1255f5d3de575dc72e
gaurav <gauravdiwaker619@gmail.com>
2021-07-10 12:29:19 +0530

    Fix Bug

--- src/app/page/faq-page/faq-page.component.ts
+++ src/app/page/faq-page/faq-page.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { faqData } from './faq-page.data';
 @Component({
   selector: 'app-faq-page',
   templateUrl: './faq-page.component.html',
   styleUrls: ['./faq-page.component.scss'],
 })
 export class FAQPageComponent implements OnInit {
+  data=faqData
   constructor() { }
 
   ngOnInit(): void {
