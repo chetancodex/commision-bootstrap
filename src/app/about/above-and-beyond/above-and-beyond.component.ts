b616e1dc5dfcf097710e2cc20f57133dd53d92e7
Your Name <you@example.com>
2021-07-12 17:58:07 +0530

    padding in %

--- src/app/about/above-and-beyond/above-and-beyond.component.ts
+++ src/app/about/above-and-beyond/above-and-beyond.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-above-and-beyond',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./above-and-beyond.component.scss']
 })
 export class AboveAndBeyondComponent implements OnInit {
-
+@Input()
+data:any;
   constructor() { }
 
   ngOnInit(): void {
